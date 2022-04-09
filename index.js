5c83e8b3b063c42aa99a68e060edecf22d82e6fb
tbpcoder <thejas.bhat11@gmail.com>
2022-04-09 15:50:24 +0530

    working to an extent

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-let siz=prompt("Enter 2 t0 64:","16");
+
+let siz=prompt("Enter 2 to 64:","16");
+
 
 buildGrid(siz);
 
 function buildGrid(siz){
     for(let i=0;i<siz;i++){
+        const p=document.createElement('div');
+        p.setAttribute('class','par');
+        const mpart=document.querySelector('.mpart');
+        mpart.append(p);
+        p.style.display='flex';
+        p.style.flexDirection='row';
+        p.style.flex='1';
         for(let j=0;j<siz;j++){
             const ele=document.createElement("div");
             ele.setAttribute('class','sub');
-            ele.style.width='20px';
-            ele.style.height='20px';
-            document.mpart.appendChild(ele);
+            ele.setAttribute('onmouseover',"changeBack(this)");
+            p.append(ele);
+            ele.style.flex='1';
+            ele.style.backgroundColor='white';
+            ele.style.border='0.005px solid black';
+
         }
     }
 }
+let ran = false;
+let b='black';
+
+const rainbow=document.querySelector('.rainbow');
+rainbow.addEventListener('click',function(e){
+    if(ran==false)
+        ran = true;
+    else
+        ran = false;
+})
+
+
+function changeBack(t){
+    if(ran==false)
+        t.style.backgroundColor=b;
+    else{
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        t.style.backgroundColor='#'+randomColor;
+    }
+}
+
+const clear=document.querySelector('.clear');
+clear.addEventListener('click',function(e){
+    document.querySelectorAll('.sub').forEach(element => {
+        element.style.backgroundColor='white';
+    })    
+})
+
+const resize=document.querySelector('.resize');
+resize.addEventListener('click',function(e){
+    window.location.reload();
+})
